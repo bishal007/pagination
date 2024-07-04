cfc5dd9b8e5cc2d8885a8ffd79bd868b0f708c4e
bishal007 <bishalsahu0680@gmail.com>
2024-07-04 11:51:56 +0530

    pagination-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import Data from './data.json'
+import { useState } from 'react';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dataPerPage] = useState(5);
+  const lastIndex = currentPage * dataPerPage;
+  const firstIndex = lastIndex - dataPerPage;
+  const currentData = Data.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(Data.length / dataPerPage);
+  const pageNumbers = [...Array(totalPages + 1).keys()].slice(1);
+  const prevPage = () => {
+    if(currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  const nextPage = () => {
+    if(currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <table>
+      <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+      </thead>
+      <tbody>
+        {currentData.map((item, index) => (
+          <tr key={index}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+          </tr>
+        ))}
+      </tbody>
+     </table>
+     <nav>
+      <ul className="pagination" style={{display:"flex", cursor:"pointer", justifyContent:"space-around" }}>
+        <div style={{display:"flex", cursor:"pointer"}}>
+          <a onClick={prevPage}>Prev</a>
+        </div>
+        {pageNumbers.map((number) => (
+          <div style={{display:"flex", cursor:"pointer"}} key={number} className={currentPage === number ? "page-item active" : "page-item"}>
+            <a onClick={() => setCurrentPage(number)} className="page-link">
+              {number}
+            </a>
+          </div>
+        ))}
+        <div style={{display:"flex", cursor:"pointer"}}>
+          <a onClick={nextPage}>Next</a>
+        </div>
+      </ul>
+     </nav>
     </div>
   );
 }
